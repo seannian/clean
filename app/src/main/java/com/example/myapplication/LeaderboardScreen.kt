1c3289fbc0be9e04146805d844d8acaf395d34e9
Angela Huang <angelahuang317@gmail.com>
2024-11-26 15:45:25 -0800

    fixed leaderboard

--- app/src/main/java/com/example/myapplication/LeaderboardScreen.kt
+++ app/src/main/java/com/example/myapplication/LeaderboardScreen.kt
@@ -4,12 +4,10 @@ import LinearProgressBar
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.ForestGreen
-import com.example.myapplication.ui.theme.Gold
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 
@@ -18,15 +16,14 @@ fun LeaderboardScreen(user: User) {
     var topUsers by remember { mutableStateOf<List<User>>(emptyList()) }
     val db = FirebaseFirestore.getInstance()
 
-    // Fetch the top 100 users when the screen is first shown
     LaunchedEffect(Unit) {
         db.collection("Users")
-            .orderBy("score", Query.Direction.DESCENDING) // Sort by score in descending order
-            .limit(100) // Limit to top 100 users
+            .orderBy("score", Query.Direction.DESCENDING)
+            .limit(100)
             .get()
             .addOnSuccessListener { snapshot ->
                 val users = snapshot.documents.mapNotNull { document ->
-                    document.toObject(User::class.java) // Convert document to User object
+                    document.toObject(User::class.java)
                 }
                 topUsers = users
             }
@@ -38,7 +35,7 @@ fun LeaderboardScreen(user: User) {
             .padding(16.dp)
     ) {
         TitleText("Global Impact Bar (60%)", 16.dp)
-        LinearProgressBar(0.6f) // Connect to database to show real progress
+        LinearProgressBar(0.6f)
         HorizontalDivider(
             color = ForestGreen,
             thickness = 5.dp,
@@ -48,10 +45,9 @@ fun LeaderboardScreen(user: User) {
         )
         TitleText("Leaderboard", 16.dp)
 
-        // Display users using LazyColumn with extra spacing between users
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(16.dp) // Adds spacing between items
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             items(topUsers.size) { index ->
                 val currentUser = topUsers[index]
