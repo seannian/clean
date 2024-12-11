cd9ff321afc9388a55dc32e88c023f34974a69ba
ETPH28 <87104268+ETPH28@users.noreply.github.com>
2024-12-10 17:06:12 -0800

    Working out conflicting gradle builds, before pr

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myapplication"
